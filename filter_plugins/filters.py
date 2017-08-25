54e5b67fa29eb9edc01c88490e7d5329fab7147f
Sam Doran <sdoran@redhat.com>
2017-08-25 13:12:19 -0400

    Also check passlib version
    The grub_pbkdf2 method was added in 1.7

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -5,6 +5,7 @@ from random import SystemRandom
 try:
     import passlib.hash
     HAS_PASSLIB = True
+    PASSLIB_VERSION = float(passlib.__version__[:3])
 except Exception as e:
     HAS_PASSLIB = False
 
@@ -16,11 +17,14 @@ def grub2_hash(password, salt=None, iterations=10000):
         r = SystemRandom()
         salt = ''.join([r.choice(string.ascii_letters + string.digits) for _ in range(64)])
 
+    # Make sure we have passlib and the correct version
     if not HAS_PASSLIB:
         raise errors.AnsibleFilterError('grub2_hash requires the passlib python module to generate password hashes')
-    else:
-        encrypted = passlib.hash.grub_pbkdf2_sha512.hash(password, salt=salt, rounds=iterations)
 
+    if PASSLIB_VERSION < 1.7:
+        raise errors.AnsibleFilterError('grub2_hash >= 1.7 is required and %s is installed' % passlib.__version__)
+
+    encrypted = passlib.hash.grub_pbkdf2_sha512.hash(password, salt=salt, rounds=iterations)
     return encrypted
 
 
