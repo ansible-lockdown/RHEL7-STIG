cef79843fe9092b4ac3549f2beb4ed517c95ea4d
Sam Doran <sdoran@ansible.com>
2017-09-07 12:45:22 -0400

    Cleanup grub2_hash filter (#23)
    * Cleanup grub2_hash filter
    
    - Remove unneceassry imports
    - Report an error istead of a stack trace if passlib is not installed
    
    Fixes #21
    
    * Add ANSIBLE_CONFIG and passlib to test containers
    
    * Also check passlib version
    
    The grub_pbkdf2 method was added in 1.7

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,35 +1,30 @@
-import crypt
-import sys
-from random import SystemRandom, shuffle
 import string
+
+from random import SystemRandom
+
 try:
     import passlib.hash
-    from passlib.hash import grub_pbkdf2_sha512
     HAS_PASSLIB = True
-except:
+    PASSLIB_VERSION = float(passlib.__version__[:3])
+except Exception as e:
     HAS_PASSLIB = False
 
-def grub2_hash(password, hashtype='sha512', salt=None):
+from ansible import errors
 
-    cryptmethod = {
-        'md5': '1',
-        'blowfish': '2a',
-        'sha256': '5',
-        'sha512': '6',
-    }
 
+def grub2_hash(password, salt=None, iterations=10000):
     if salt is None:
         r = SystemRandom()
         salt = ''.join([r.choice(string.ascii_letters + string.digits) for _ in range(64)])
 
+    # Make sure we have passlib and the correct version
     if not HAS_PASSLIB:
-        if sys.platform.startswith('darwin'):
-            raise errors.AnsibleFilterError('|password_hash requires the passlib python module to generate password hashes on Mac OS X/Darwin')
-        saltstring =  "$%s$%s" % (cryptmethod[hashtype],salt)
-        encrypted = crypt.crypt(password, salt=salt)
-    else:
-        encrypted = grub_pbkdf2_sha512.hash(password)
+        raise errors.AnsibleFilterError('grub2_hash requires the passlib python module to generate password hashes')
 
+    if PASSLIB_VERSION < 1.7:
+        raise errors.AnsibleFilterError('grub2_hash >= 1.7 is required and %s is installed' % passlib.__version__)
+
+    encrypted = passlib.hash.grub_pbkdf2_sha512.hash(password, salt=salt, rounds=iterations)
     return encrypted
 
 
