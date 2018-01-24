df72d02aaa8d20bffd2270a2c61266af23cbb16b
Daniel Shepherd <shepdelacreme@users.noreply.github.com>
2018-01-24 12:55:29 -0500

    grub2_hash support for older (<1.7) passlib versions (#59)
    * update to support older passlib versions back to 1.5 and protect against deprecated encrypt() method being removed eventually, remove unneeded libs and random salt generation code and use default random salt from passlib, use kwargs to allow for passing in suer specific values
    
    * use ImportError instead of generic Exception here

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,30 +1,38 @@
-import string
-
-from random import SystemRandom
-
 try:
     import passlib.hash
     HAS_PASSLIB = True
     PASSLIB_VERSION = float(passlib.__version__[:3])
-except Exception as e:
+except ImportError as e:
     HAS_PASSLIB = False
 
 from ansible import errors
 
 
-def grub2_hash(password, salt=None, iterations=10000):
-    if salt is None:
-        r = SystemRandom()
-        salt = ''.join([r.choice(string.ascii_letters + string.digits) for _ in range(64)])
+def grub2_hash(password, salt=None, iterations=25000):
+    # Build list of keyword arguments to pass into hash/encrypt method
+    kwargs = dict()
+
+    if salt:
+        # passlib generates a random salt of 64 byte length by default (recommended)
+        kwargs['salt'] = salt.encode()
+
+    if iterations:
+        # passlib does 25000 rounds (in version 1.7) by default (recommended or set to a higher value)
+        kwargs['rounds'] = iterations
 
     # Make sure we have passlib and the correct version
     if not HAS_PASSLIB:
         raise errors.AnsibleFilterError('grub2_hash requires the passlib python module to generate password hashes')
 
-    if PASSLIB_VERSION < 1.7:
-        raise errors.AnsibleFilterError('grub2_hash >= 1.7 is required and %s is installed' % passlib.__version__)
+    if PASSLIB_VERSION < 1.5:
+        raise errors.AnsibleFilterError('passlib >= 1.5 is required and %s is installed' % PASSLIB_VERSION)
 
-    encrypted = passlib.hash.grub_pbkdf2_sha512.hash(password, salt=salt.encode(), rounds=iterations)
+    elif PASSLIB_VERSION < 1.7:
+        encrypted = passlib.hash.grub_pbkdf2_sha512.encrypt(password, **kwargs)
+
+    else:
+        encrypted = passlib.hash.grub_pbkdf2_sha512.using(**kwargs).hash(password)
+
     return encrypted
 
 
