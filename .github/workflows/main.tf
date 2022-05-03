31e5d35ecd2f56fac0661ae14f5b6357697654d2
Mark Bolwell <mark.bollyuk@gmail.com>
2022-05-03 11:54:25 +0100

    added passwd hash to inv
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -78,6 +78,6 @@ resource "local_file" "inventory" {
         run_audit: true
         system_is_ec2: true
         audit_git_version: devel
+        rhel7stig_bootloader_password_hash: thishasbeenchangedfortesting
     EOF
 }
-
